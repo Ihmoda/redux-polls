ff5a85b1ea677a5fb9fc9adc466e97f9436444b7
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-03-16 11:24:17 -0700

    cleaned up add

--- src/components/Add.js
+++ src/components/Add.js
@@ -5,27 +5,40 @@ import { handleAddData } from "../actions/shared";
 class Add extends Component {
   constructor(props) {
     super(props);
-
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      question: "",
+      a: "",
+      b: "",
+      c: "",
+      d: "",
+      author: this.props.authedUser
+    };
   }
 
-  handleSubmit(event) {
+  handleSubmit = event => {
     event.preventDefault();
     const { dispatch, authedUser, history } = this.props;
 
-    const newPoll = {
-      question: this.question.value,
-      a: this.optionA.value,
-      b: this.optionB.value,
-      c: this.optionC.value,
-      d: this.optionD.value,
-      author: authedUser
-    };
-
-    dispatch(handleAddData(newPoll, this.props.authedUser));
-
+    dispatch(handleAddData(this.state, this.props.authedUser));
     history.push("/");
-  }
+  };
+
+  handleOnChange = event => {
+    const { name, value } = event.target;
+
+    this.setState(() => ({
+      [name]: value
+    }));
+  };
+
+  isDisabled = () => {
+    return (
+      this.state.a === "" ||
+      this.state.b === "" ||
+      this.state.c === "" ||
+      this.state.d === ""
+    );
+  };
 
   render() {
     return (
@@ -33,36 +46,41 @@ class Add extends Component {
         <form className="addform" onSubmit={this.handleSubmit}>
           <h3>What is your question?</h3>
           <input
-            ref={input => (this.question = input)}
+            onChange={this.handleOnChange}
+            name="question"
             type="text"
             className="input"
           />
           <h3>What are the options?</h3>
           <h3>A.</h3>
           <input
-            ref={input => (this.optionA = input)}
+            onChange={this.handleOnChange}
+            name="a"
             type="text"
             className="input"
           />
           <h3>B.</h3>
           <input
-            ref={input => (this.optionB = input)}
+            onChange={this.handleOnChange}
+            name="b"
             type="text"
             className="input"
           />
           <h3>C.</h3>
           <input
-            ref={input => (this.optionC = input)}
+            onChange={this.handleOnChange}
+            name="c"
             type="text"
             className="input"
           />
           <h3>D.</h3>
           <input
-            ref={input => (this.optionD = input)}
+            onChange={this.handleOnChange}
+            name="d"
             type="text"
             className="input"
           />
-          <button type="submit" className="btn">
+          <button type="submit" className="btn" disabled={this.isDisabled()}>
             Submit
           </button>
         </form>
